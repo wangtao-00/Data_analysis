36d12753de991c07144771e10610717970ecfb0a
wangtao-00 <62607945+wangtao-00@users.noreply.github.com>
2023-12-20 15:56:05 +0800

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 import home_page
 import frequency_distribution_page
 import pie_bar_chart_page
+import decision_tree_page
 
 st.set_page_config(page_title="数据分析", page_icon=":tiger:", layout="wide")
 
 PAGES = {
     "主页": home_page,
     "频率分布图": frequency_distribution_page,
-    "饼状图和直方图": pie_bar_chart_page
+    "饼状图和直方图": pie_bar_chart_page,
+    "决策树":decision_tree_page
 }
 
 st.sidebar.title('导航')
