7afc6bae6a198737bb9f5b7bfd04482d8ad0cd68
wangtao-00 <62607945+wangtao-00@users.noreply.github.com>
2023-12-20 16:15:01 +0800

    decision_tree_page.py

--- decision_tree_page.py
+++ decision_tree_page.py
@@ -6,9 +6,31 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+from streamlit_lottie import st_lottie
+
+def load_lottie(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 def app():
     st.title("决策树模型")
+    lottie_url = 'https://lottie.host/72c56b42-152e-47d9-9502-a631e7ff7afe/v6bJrr0uS7.json'
+    lottie_animation1 = load_lottie(lottie_url)
+    with st.container():
+        text_column,image_column = st.columns((2, 1))
+        with text_column:
+            st.write("""
+                决策树：从训练数据中学习得出一个树状结构的模型。
+                    决策树属于判别模型。
+                    - 决策树是一种树状结构，通过做出一系列决策（选择）来对数据进行划分，这类似于针对一系列问题进行选择。
+                    - 决策过程就是从根节点开始，测试待分类项中对应的特征属性，并按照其值选择输出分支，
+                    - 直到叶子节点，将叶子节点的存放的类别作为决策结果。
+                        """)
+        with image_column:
+            st_lottie(lottie_animation1, height=150, key='coding')
 
     # 文件上传
     uploaded_file = st.file_uploader("上传 EEG Eye State 数据文件", type=["txt"])
