8f0f15e30068b1f9a152b19fe4f441de9fff6c21
wangtao-00 <62607945+wangtao-00@users.noreply.github.com>
2024-01-02 20:31:39 +0800

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import home_page
 import frequency_distribution_page
 import pie_bar_chart_page
 import decision_tree_page
-import mninst
 import script_name
 st.set_page_config(page_title="数据分析", page_icon=":tiger:", layout="wide")
 
@@ -12,7 +11,6 @@ PAGES = {
     "频率分布图": frequency_distribution_page,
     "饼状图和直方图": pie_bar_chart_page,
     "决策树":decision_tree_page,
-    "手写字体识别":mninst,
     "打卡":script_name,
 }
 
